267c6b1b9aba708a76f9778cdc7173b7ea7decf2
ethyi <ethyi@proton.me>
2022-07-13 20:06:55 -0700

    Database setup, initial tests out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use newsletter::configuration::get_configuration;
+use newsletter::startup::run;
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to get config");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to postgres");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection)?.await
 }
